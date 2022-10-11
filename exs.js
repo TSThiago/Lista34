190baf3d263d739b366d29f182a7cdb5dd12d1ce
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-10-10 23:08:34 -0300

    Término do exercício 3.

--- exs.js
+++ exs.js
@@ -4,20 +4,36 @@
 //     return palavra.endsWith(final)
 // }
 // Ex 2:
-console.log(SubstituirCaractere("teste"));
-function SubstituirCaractere(palavra) {
-    var novaPalavra = palavra.match(/\w/gi);
-    console.log(novaPalavra);
-    var arrayPalavra = palavra.split('');
-    var arrayFinal = [];
-    arrayPalavra.forEach(function (x) {
-        if (arrayPalavra.filter(function (letra) { return letra == x; }).length > 1) {
-            arrayFinal.push(")");
-        }
-        else {
-            arrayFinal.push("(");
-        }
+// console.log(SubstituirCaractere("teste"))
+// function SubstituirCaractere(palavra : string){
+//     let arrayPalavra : string[] = palavra.split('')
+//     let arrayFinal : string[] = []
+//     arrayPalavra.forEach(x => {
+//         if(arrayPalavra.filter(letra => letra == x).length > 1){
+//             arrayFinal.push(")")   
+//         }else{
+//             arrayFinal.push("(")
+//         }
+//     })
+//     console.log(arrayFinal)
+//     return arrayFinal.join('')
+// }
+// Ex 3:
+console.log(ContarDigito(11, "6"));
+function ContarDigito(n, d) {
+    var contador = 0;
+    var arrayQuadrado = [];
+    for (var index = 0; index <= n; index++) {
+        arrayQuadrado.push(index * index);
+    }
+    arrayQuadrado.forEach(function (x) {
+        var numeroArray = x.toString().split('').filter(function (numero) { return x; });
+        numeroArray.forEach(function (y) {
+            if (d == y) {
+                contador++;
+            }
+        });
     });
-    console.log(arrayFinal);
-    return arrayFinal.join('');
+    console.log(arrayQuadrado);
+    return contador;
 }
--- exs.ts
+++ exs.ts
@@ -8,21 +8,42 @@
 
 // Ex 2:
 
-console.log(SubstituirCaractere("teste"))
+// console.log(SubstituirCaractere("teste"))
 
-function SubstituirCaractere(palavra : string){
-    let arrayPalavra : string[] = palavra.split('')
-    let arrayFinal : string[] = []
-    arrayPalavra.forEach(x => {
-        if(arrayPalavra.filter(letra => letra == x).length > 1){
-            arrayFinal.push(")")   
-        }else{
-            arrayFinal.push("(")
-        }
+// function SubstituirCaractere(palavra : string){
+//     let arrayPalavra : string[] = palavra.split('')
+//     let arrayFinal : string[] = []
+//     arrayPalavra.forEach(x => {
+//         if(arrayPalavra.filter(letra => letra == x).length > 1){
+//             arrayFinal.push(")")   
+//         }else{
+//             arrayFinal.push("(")
+//         }
+//     })
+//     console.log(arrayFinal)
+//     return arrayFinal.join('')
+// }
+
+// Ex 3:
+
+console.log(ContarDigito(11, "6"))
+
+function ContarDigito(n : number, d : string) : number{
+    let contador : number = 0
+    let arrayQuadrado : number[] = []
+    for(let index : number = 0; index <= n; index++){
+        arrayQuadrado.push(index * index)
+    }
+    arrayQuadrado.forEach(x => {
+        let numeroArray : string[] = x.toString().split('').filter(numero => x)
+        numeroArray.forEach(y => {
+            if(d == y){
+                contador++
+            }
+        })
     })
-    console.log(arrayFinal)
-    return arrayFinal.join('')
+    console.log(arrayQuadrado)
+    return contador
 }
-
 
 

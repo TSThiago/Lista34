3f953fde9acd5a249948a2f80a1afc14d2e2538c
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-10-10 22:57:27 -0300

    Término do exercício 2.

--- exs.js
+++ exs.js
@@ -1,5 +1,23 @@
 // Ex 1:
-console.log(VerificarFinalDaPalavra("estudo", "123"));
-function VerificarFinalDaPalavra(palavra, final) {
-    return palavra.endsWith(final);
+// console.log(VerificarFinalDaPalavra("estudo","123"))
+// function VerificarFinalDaPalavra(palavra : string, final : string) : boolean{
+//     return palavra.endsWith(final)
+// }
+// Ex 2:
+console.log(SubstituirCaractere("teste"));
+function SubstituirCaractere(palavra) {
+    var novaPalavra = palavra.match(/\w/gi);
+    console.log(novaPalavra);
+    var arrayPalavra = palavra.split('');
+    var arrayFinal = [];
+    arrayPalavra.forEach(function (x) {
+        if (arrayPalavra.filter(function (letra) { return letra == x; }).length > 1) {
+            arrayFinal.push(")");
+        }
+        else {
+            arrayFinal.push("(");
+        }
+    });
+    console.log(arrayFinal);
+    return arrayFinal.join('');
 }
--- exs.ts
+++ exs.ts
@@ -1,7 +1,28 @@
 // Ex 1:
 
-console.log(VerificarFinalDaPalavra("estudo","123"))
+// console.log(VerificarFinalDaPalavra("estudo","123"))
 
-function VerificarFinalDaPalavra(palavra : string, final : string) : boolean{
-    return palavra.endsWith(final)
+// function VerificarFinalDaPalavra(palavra : string, final : string) : boolean{
+//     return palavra.endsWith(final)
+// }
+
+// Ex 2:
+
+console.log(SubstituirCaractere("teste"))
+
+function SubstituirCaractere(palavra : string){
+    let arrayPalavra : string[] = palavra.split('')
+    let arrayFinal : string[] = []
+    arrayPalavra.forEach(x => {
+        if(arrayPalavra.filter(letra => letra == x).length > 1){
+            arrayFinal.push(")")   
+        }else{
+            arrayFinal.push("(")
+        }
+    })
+    console.log(arrayFinal)
+    return arrayFinal.join('')
 }
+
+
+
